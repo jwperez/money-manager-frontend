845c56e1e1f1aa4f66eb240dfbfc5cc26c3afc51
JP <jonathan.perez@me.com>
2018-10-09 15:00:21 -0400

    added Budget component

--- money-manager-frontend/src/App.js
+++ money-manager-frontend/src/App.js
@@ -3,11 +3,45 @@ import logo from './logo.svg';
 import './App.css';
 import Budget from "./components/Budget.js";
 
+const budget = {
+    incomeStreams: [
+        {
+            name: "Paycheck",
+            amount: 2000,
+            frequency: 2
+        },
+        {
+            name: "Consulting",
+            amount: 300,
+            frequency: 1
+        }
+    ],
+    expenses: [
+        {
+            name: "Mortgage",
+            amount: 1300
+        },
+        {
+            name: "Internet",
+            amount: 80
+        },
+        {
+            name: "Phone",
+            amount: 120
+        },
+        {
+            name: "Credit Card",
+            amount: 1000
+        }
+
+    ]
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Budget />
+        <Budget {...budget} />
       </div>
     );
   }
--- money-manager-frontend/src/components/Budget.js
+++ money-manager-frontend/src/components/Budget.js
@@ -1,9 +1,45 @@
 import React, { Component } from "react";
 import "./budget.css";
 
+class IncomeStream extends Component {
+    render() {
+        return (
+            <tr>
+                <td>{this.props.name}</td>
+                <td>{this.props.amount}</td>
+                <td>{this.props.frequency}</td>
+            </tr>
+        );
+    }
+}
+
+class Expense extends Component {
+    render() {
+        return (
+            <tr>
+                <td>{this.props.name}</td>
+                <td>{this.props.amount}</td>
+            </tr>
+        );
+    }
+}
+
 class Budget extends Component {
     state = {};
     render() {
+
+        const incomeStreamComponents = this.props.incomeStreams.map(incomeStreamObject => {
+            return (
+                <IncomeStream {...incomeStreamObject} />
+            )
+        })
+
+        const expenseStreamComponents = this.props.expenses.map(expensesStreamObject => {
+            return (
+                <Expense {...expensesStreamObject} />
+            )
+        })
+
         return (
             <div className="budget">
                 <h1>Budget</h1>
@@ -11,16 +47,14 @@ class Budget extends Component {
                     <h2>Income Stream</h2>
                     <table>
                         <thead>
-                        <th>Name</th>
-                        <th>Amount</th>
-                        <th>Frequency</th>
+                            <tr>
+                                <th>Name</th>
+                                <th>Amount</th>
+                                <th>Frequency</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        <tr>
-                            <td>Paycheck</td>
-                            <td>$2000</td>
-                            <td>2/month</td>
-                        </tr>
+                            {incomeStreamComponents}
                         </tbody>
                     </table>
                 </div>
@@ -34,26 +68,7 @@ class Budget extends Component {
                         <th>Frequency</th>
                         </thead>
                         <tbody>
-                        <tr>
-                            <td>Mortgage</td>
-                            <td>$1300</td>
-                            <td>1/month</td>
-                        </tr>
-                        <tr>
-                            <td>Internet</td>
-                            <td>$60</td>
-                            <td>1/month</td>
-                        </tr>
-                        <tr>
-                            <td>Cell Phone</td>
-                            <td>$200</td>
-                            <td>1/month</td>
-                        </tr>
-                        <tr>
-                            <td>Credit Card</td>
-                            <td>$60</td>
-                            <td>1/month</td>
-                        </tr>
+                            {expenseStreamComponents}
                         </tbody>
                     </table>
                 </div>
